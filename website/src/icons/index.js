7221e91c960adbe59f7e0a2d0805b6481a5088dc
Bharat Chauhan <tell.bhch@gmail.com>
2020-11-10 10:51:17 +0530

    Move svg icons to separate files

---
+++ website/src/icons/index.js
@@ -0,0 +1,10 @@
+import {ReactComponent as clipboard} from './clipboard.svg';
+import {ReactComponent as search} from './search.svg';
+
+
+const icons = {
+    clipboard: clipboard,
+    search: search
+};
+
+export default icons;
